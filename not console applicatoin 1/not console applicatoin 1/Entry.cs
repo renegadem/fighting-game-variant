e79b08aa0adde567e15df4bb5feaa4ca85f4cd00
renegadem <thebalzernator@gmail.com>
2016-09-03 21:11:16 -0500

    working game

---
+++ not console applicatoin 1/not console applicatoin 1/Entry.cs
@@ -0,0 +1,82 @@
+﻿using System;   //Console.WriteLine
+//      ".ReadKey
+using System.Diagnostics; //StopWatch
+using System.IO;//StreamWriter
+using System.Threading;
+
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace PlatformerGame
+{
+    class Entry
+    {
+        [STAThread]
+        public static void Main(string[] aArgs)
+        {
+            Vector2i activeResolution = new Vector2i(800, 600);
+            RenderWindow rw = new RenderWindow(new VideoMode((uint)activeResolution.X, (uint)activeResolution.Y), "2DEngine");
+            rw.Position = new Vector2i(0, 0);
+            rw.SetActive(true);
+            rw.SetVerticalSyncEnabled(true);
+            rw.Closed += rw_Closed;
+            
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            long elapsed = 20;
+            DateTime tempTime1, tempTime2;
+            Screen lTestScreen = new Screen();
+            using (StreamWriter sw = new StreamWriter("LOG.txt"))
+                while (rw.IsOpen)
+                {
+                    rw.DispatchEvents();
+
+                    //TODO > 
+                    // This is borked for the tiles and IDK why. Currently this is disabled so I don't
+                    // see the flashing tiles and popup menus, they are annoying.
+                    //
+                    // I think maybe I could ask Nathan Bean about this, see if he has any idea's for why
+                    // this is happening. Maybe talk to matt about it and see his opinion.
+                    //
+                    // Never mind, now it is happening regardless.
+                    //
+                    // The log i created does not show that the draw times are lagging, nor are the update speeds.
+                    // Maybe it has to do with something someone once told me about: double buffering. I should
+                    // look into this specifically to see if maybe that is what the root is.
+                    if (elapsed > 20)
+                    {
+                        timer.Restart();
+
+                        tempTime1 = DateTime.Now;
+                        sw.WriteLine("    DRAW START TIME : {0}", tempTime1.Millisecond);
+                        lTestScreen.update(rw, elapsed);
+                        //game.update(rw, elapsed);
+                        tempTime2 = DateTime.Now;
+                        sw.WriteLine("      DRAW END TIME : {0}", tempTime2.Millisecond);
+                        sw.WriteLine("  DRAW ELAPSED TIME : {0}", tempTime2.Millisecond - tempTime1.Millisecond);
+
+                        tempTime1 = DateTime.Now;
+                        sw.WriteLine("  UPDATE START TIME : {0}", tempTime1.Millisecond);
+                        lTestScreen.draw(rw, elapsed);
+                        //game.draw(rw, elapsed);
+                        tempTime2 = DateTime.Now;
+                        sw.WriteLine("    UPDATE END TIME : {0}", tempTime2.Millisecond);
+                        sw.WriteLine("UPDATE ELAPSED TIME : {0}", tempTime2.Millisecond - tempTime1.Millisecond);
+                        sw.WriteLine(); 
+                    }
+
+                    elapsed = timer.ElapsedMilliseconds;
+                }
+
+            Console.WriteLine("\nPlease press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void rw_Closed(object sender, EventArgs e)
+        {
+            ((RenderWindow)sender).Close();
+        }
+    }
+}
---
+++ not console applicatoin 1/not console applicatoin 1/Screen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace PlatformerGame
+{
+    class Screen
+    {
+        Image image;
+        Texture texture;
+        Texture bgTex;
+        Sprite bg;
+        Sprite platform;
+
+        Texture charTex;
+        Sprite characterPlaceholder;
+
+
+        public Screen()
+        {
+            image = new Image(700, 100, Color.White);
+            bgTex = new Texture(new Image(@"C:\Users\balzernator\Documents\visual studio 2012\Projects\not console applicatoin 1\not console applicatoin 1\assets\bg.jpg"));
+            bg = new Sprite(bgTex);
+            texture = new Texture(image);
+            platform = new Sprite(texture);
+            platform.Position = new Vector2f(50, 400);
+            charTex = new Texture(new Image(50, 100, Color.Red));
+            characterPlaceholder = new Sprite(charTex);
+            characterPlaceholder.Position = new Vector2f(100, 300);
+        }
+
+        public void update(RenderWindow rw, long elapsed)
+        {
+            Console.WriteLine("updating");
+            Console.WriteLine(platform.GetGlobalBounds());
+
+            characterPlaceholder.Position = new Vector2f(characterPlaceholder.Position.X + 1, characterPlaceholder.Position.Y);
+        }
+
+        public void draw(RenderWindow rw, long elapsed)
+        {
+            Console.WriteLine("drawing");
+            rw.Clear(Color.Black);
+            rw.Draw(bg);
+            rw.Draw(platform);
+            rw.Draw(characterPlaceholder);
+            rw.Display();
+        }
+    }
+}
